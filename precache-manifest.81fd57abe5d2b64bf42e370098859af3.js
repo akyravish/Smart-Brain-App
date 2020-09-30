34a915cb262961c615223badab31ac6b90633858
akyravish <aravish96@gmail.com>
2020-09-30 18:32:52 +0530

    Updates

---
+++ precache-manifest.81fd57abe5d2b64bf42e370098859af3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d288c8c7e5b2ddc1abc2154b468f6e8c",
+    "url": "/Smart-Brain-App/index.html"
+  },
+  {
+    "revision": "df26d84245279c683776",
+    "url": "/Smart-Brain-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e513b1afa93c483aab6a",
+    "url": "/Smart-Brain-App/static/css/main.d7064784.chunk.css"
+  },
+  {
+    "revision": "df26d84245279c683776",
+    "url": "/Smart-Brain-App/static/js/2.43aa69cc.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/Smart-Brain-App/static/js/2.43aa69cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e513b1afa93c483aab6a",
+    "url": "/Smart-Brain-App/static/js/main.6fd6d465.chunk.js"
+  },
+  {
+    "revision": "c5b1a6a677bf9db371e1",
+    "url": "/Smart-Brain-App/static/js/runtime-main.986edc9f.js"
+  },
+  {
+    "revision": "4100098c8af8b04589771aa58935f4c8",
+    "url": "/Smart-Brain-App/static/media/Icon.4100098c.png"
+  }
+]);
